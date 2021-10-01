f8000c70602d734a479311c58e077294c266addc
quinntoye0 <4toyeq57@solent.ac.uk>
2021-10-01 12:26:24 +0100

    Updated code to display a message with escape characters

--- Basics/Output/simple_message.py
+++ Basics/Output/simple_message.py
@@ -1,2 +1,5 @@
 # Display message
-print("\nSystem Failure Imminent\nSystem reboot has been initiated...")
+print("\nSystem Failure Imminent")
+print("\nSystem reboot has been initiated..."
+      "\n\n...rebooting sensory systems\n...rebooting output motors\n...rebooting hover engine\n\nSystem online")
+print("\nI am Beep!")
